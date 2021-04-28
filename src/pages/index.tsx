5706e3c1a6876093edb03dbe6d7d66373fd4b9f5
ftang-cfa <ftang@codeforamerica.org>
2021-04-27 18:37:29 -0700

    update import filepath in _app.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,6 @@
 import Head from 'next/head'
 
 export default function Home() {
-<<<<<<< HEAD
-  return (
-    <div className="flex justify-center">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <div className="mt-4 p-4 w-1/4 rounded bg-blue-300 text-center">
-        <p className="text-blue-600">This should be very blue.</p>
-      </div>
-    </div>
-  )
-}
-=======
     return (
       <div className="flex justify-center">
         <Head>
@@ -29,4 +14,3 @@ export default function Home() {
       </div>
     )
   }
->>>>>>> 57197536a95b3cf6f165b990a56bea52a730f822
